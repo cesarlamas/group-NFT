1003fb7c190793c235d4a92f47df6a4b52b8fa2d
cesarlamas <you@example.com>
2021-12-11 11:25:45 +0000

    Added a Procfile.

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,14 @@ const cors = require("cors")
 const router = require("./router")
 const app = express()
 
+const PORT = process.env.PORT || 3001
+
 app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(3001, () => {
+if (process.env.NODE_ENV === "production")
+
+app.listen(PORT, () => {
     console.log("App is listening on Port 3001")
 })
